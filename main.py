5feb5601b3f90c077b45462a1c7099c8f344db5b
amirhossein-rajabpour <rajabpouramirhosein@gmail.com>
2021-11-18 23:18:04 +0330

    readind data with pandas and numpy

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 # Author: Amirhoseein Rajabpour 9731085
 # Information Retrieval project - Fall 2021
 import preprocessing
+import pandas as pd
 
 if __name__ == '__main__':
     # TODO 1: read excel file (with pandas and numpy)
     #         takes excel file as input --> outputs a numpy array
-    array_of_docs = []
+    docs_df = pd.read_excel("IR1_7k_news.xlsx")
+    array_of_docs = docs_df.to_numpy()
     array_of_docs = preprocessing.preprocessing(array_of_docs)
 
     # TODO 2: call functions for pre-processing
@@ -16,4 +18,3 @@ if __name__ == '__main__':
 
     # TODO 4: write some functions to handle clients queries
     #         takes query --> outputs a list of related doc titles
-    pass
