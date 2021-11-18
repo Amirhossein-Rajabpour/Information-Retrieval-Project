33b8990343e2f9347867aba39e79d8ff2db66b41
amirhossein-rajabpour <rajabpouramirhosein@gmail.com>
2021-11-18 21:21:15 +0330

    created main and preprocessing files (basic structure)

---
+++ main.py
@@ -0,0 +1,19 @@
+# Author: Amirhoseein Rajabpour 9731085
+# Information Retrieval project - Fall 2021
+import preprocessing
+
+if __name__ == '__main__':
+    # TODO 1: read excel file (with pandas and numpy)
+    #         takes excel file as input --> outputs a numpy array
+    array_of_docs = []
+    array_of_docs = preprocessing.preprocessing(array_of_docs)
+
+    # TODO 2: call functions for pre-processing
+    #         takes numpy array as input --> outputs a pre-processed numpy array
+
+    # TODO 3: create positional postings lists (and other necessary objects)
+    #         takes a pre-processed numpy array in --> outputs positional postings lists
+
+    # TODO 4: write some functions to handle clients queries
+    #         takes query --> outputs a list of related doc titles
+    pass
