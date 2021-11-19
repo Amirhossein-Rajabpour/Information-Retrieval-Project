a4b0071fb0702b51dff703f92721914dbbe36786
amirhossein-rajabpour <rajabpouramirhosein@gmail.com>
2021-11-19 22:12:40 +0330

    Document class added

--- main.py
+++ main.py
@@ -4,11 +4,21 @@ import lists
 import pandas as pd
 import preprocessing
 
+class Document:
+    def __init__(self, id, title, body, url):
+        self.id = id
+        self.title = title
+        self.body = body
+        self.url = url
+
+
 if __name__ == '__main__':
     # TODO 1: read excel file (with pandas and numpy)
     #         takes excel file as input --> outputs a numpy array
     docs_df = pd.read_excel("IR1_7k_news.xlsx")
     array_of_docs = docs_df.to_numpy()
+
+    # TODO: create list of Document objects
 
     # TODO 2: call functions for pre-processing
     #         takes numpy array as input --> outputs a pre-processed numpy array
