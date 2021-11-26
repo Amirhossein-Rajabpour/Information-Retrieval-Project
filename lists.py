f2c58e5eeff232c87117e6c01fc7727c15ee351f
amirhossein-rajabpour <rajabpouramirhosein@gmail.com>
2021-11-26 18:40:01 +0330

    modified positional_index

--- lists.py
+++ lists.py
@@ -1,4 +1,5 @@
 import numpy as np
+from main import *
 
 
 # it returns the positions of that term in that doc
@@ -17,29 +18,34 @@ def find_positions_in_doc(term, doc):
 #   word2: [frequency in all docs, {doc1: (freq in doc1, [positions in doc1]), doc2:(freq in doc2, [positions in doc2]), ...}],
 #   ...
 # }
-def create_positional_index(array_of_docs):     # we need doc id
+def create_positional_index(array_of_docs):
     positional_index = {}
     for doc in array_of_docs:
-        for term in doc.body:     # the same for doc.title
-            positions_in_doc = find_positions_in_doc(term, doc.body)
-            if term not in positional_index.keys():
+        for term in doc.content:
+            if term not in positional_index.keys():     # we are evaluating a new term (this is the first time that this term has appeared in all docs)
+                positions_in_doc = find_positions_in_doc(term, doc.content)
                 positions_dictionary = {doc.id: (len(positions_in_doc), positions_in_doc)}  # positions_dictionary didn't exist before for this term
                 positional_index[term] = [len(positions_in_doc), positions_dictionary]  # add the new term to the positional_index dictionary
-            else:
-                # TODO: here we should check that each term should be evaluated in each doc only once!!
-                positions_dictionary[doc.id] = (len(positions_in_doc), positions_in_doc)    # positions_dictionary exists for this term and should be updated
-                # TODO: test
+            elif term in positional_index.keys() and doc.id in positional_index.get(term)[1].keys():
+                pass    # everything is already calculated for this term (this term appeared in this doc more than once)
+            else:   # we had this term before in other docs but it is new in this doc
+                positions_in_doc = find_positions_in_doc(term, doc.content)
+                positional_index.get(term)[1][doc.id] = (len(positions_in_doc), positions_in_doc)    # TODO: positions_dictionary exists for this term and should be updated (error)
                 positional_index[term] = [positional_index.get(term)[0] + len(positions_in_doc), positions_dictionary]  # update the whole frequency of that term and its new positions_dictionary (with new doc)
 
     return positional_index
 
 
 if __name__ == '__main__':
-    tmp_arr = np.array([[['اصلاح', 'کتاب', 'و', 'استفاده', 'از', 'نیم\u200cفاصله', 'پرداز', 'را', 'آس', 'می\u200cکند'],
-                         [0],
-                         ['ما', 'ه', 'برا', 'وصل', 'کردن', 'آمدیم!', 'ول', 'برا', 'پردازش،', 'جدا', 'به', 'نیست؟']],
-                        [[], [0], []]],
-                       dtype=object, )
+    tmp_arr = np.array(['ما', 'ه', 'ما'])
+    tmp_arr2 = np.array(['ما', 'ه', 'ما', 'برا', 'وصل', 'کردن', 'آمدیم!', 'ول', 'برا', 'پردازش،', 'جدا', 'به', 'نیست؟','اصلاح', 'کتاب', 'و', 'استفاده', 'از', 'نیم\u200cفاصله', 'پرداز', 'را', 'آس', 'می\u200cکند'])
 
-    tmp_positional_index = create_positional_index(tmp_arr)
+    array_of_docs = []
+    document1 = Document(id=111, title="title", content=tmp_arr, url="url")
+    document2 = Document(id=222, title="title2", content=tmp_arr2, url="url2")
+
+    array_of_docs.append(document1)
+    array_of_docs.append(document2)
+
+    tmp_positional_index = create_positional_index(array_of_docs)
     print("positional index:\n", tmp_positional_index)
