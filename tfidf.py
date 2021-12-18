8497a5d2c8dd89cf508d2be6bce6043c1c264bb7
amirhossein-rajabpour <rajabpouramirhosein@gmail.com>
2021-12-18 18:49:53 +0330

    added similarity scores dictionary

--- tfidf.py
+++ tfidf.py
@@ -1,20 +1,24 @@
 import math
-
-# TODO: calculate idf for each term
-def calculate_idf(collection_size, term):   # term is object
-    return math.log10(collection_size / len(term.freq_in_each_doc))     # N/num of docs containing t
+import hazm
+from process_query import query_stemming
 
 
-# TODO: calculate term frequency (tf) for a given term. term is object
+# calculate idf for each term
+def calculate_idf(collection_size, term):  # term is object
+    return math.log10(collection_size / len(term.freq_in_each_doc))  # N/num of docs containing t
+
+
+# calculate term frequency (tf) for a given term. term is object
 def calculate_tf(term, doc):
-    # return frequency of the term in that doc
     return term.freq_in_each_doc.get(doc.id)
 
 
 # TODO: calculate cos similarity
 def cos_similarity(query, doc):
     # return cos_score between query and doc
-    pass
+    score = 0
+    return score
+
 
 # TODO: create champion list
 def create_champion_list(terms):
@@ -22,10 +26,34 @@ def create_champion_list(terms):
 
 
 def tf_idf(query, terms, collection):
-
     for doc in collection:  # create a term score vector for each doc
         seen_terms = []
-        for term in doc.content:    # here term is string
+        for term in doc.content:  # here term is string
             if term not in seen_terms:  # avoid calculating tfidf more than one time for each term in doc
                 doc.term_scores[term] = (1 + math.log10(calculate_tf(terms.get(term), doc))) * calculate_idf(len(collection), terms.get(term))
                 seen_terms.append(term)
+
+    # first we should preprocess the query like our dataset
+    normalizer = hazm.Normalizer()
+    stemmer = hazm.Stemmer()
+    query = normalizer.normalize(query)
+    splitted_query = query.split(" ")
+    query = query_stemming(splitted_query, stemmer)
+    seen_terms_query = []
+    query_scores = {}
+    for term in query:  # here term is string
+        if term not in seen_terms_query:  # avoid calculating tfidf more than one time for each term in doc
+            # TODO: how to calculate term frequency in query?
+            query_scores[term] = (1 + math.log10(calculate_tf(terms.get(term), query))) * calculate_idf(len(collection),terms.get(term))
+            seen_terms_query.append(term)
+
+    similarities = {}  # {doc: similarity}
+    for doc in collection:
+        similarities[doc] = cos_similarity(query_scores, doc.term_scores)
+
+    # sort scores dictionary
+    similarities = dict(sorted(similarities.items(), key=lambda item: item[1], reverse=True))
+
+    # return top K docs
+    k = 5
+    first_K_pairs = {k: similarities[k] for k in list(similarities)[:k]}
